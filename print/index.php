a1eba2cc7c382aae7d7299f63d5359b3447eaa75
heiko.valli <heiko.valli@khk.ee>
2018-12-13 13:07:14 +0200

    Väljastamine

---
+++ print/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heiko.valli
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+print ('Tere maailm<br>');
+print ('PHP Alused<br>');
+echo 'Toimub väljastamine<br>';
+// echo 'asd'
+
+// väärtuste ühendamine.
+echo 'See '.' on '.' kolmest sõnast kokku pandud <br>';
