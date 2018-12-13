74898cdd3c67ca9e90d0ad3d5638ae8751abfe16
heiko.valli <heiko.valli@khk.ee>
2018-12-13 13:24:07 +0200

    Muutujad

---
+++ Variables/variables.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heiko.valli
+ * Date: 13.12.2018
+ * Time: 13:09
+ */
+
+$taisArv = 3;
+$reaalArv = 3.3;
+$sone = 'kolm';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+// väljastame muutujad koos selgitustega
+echo 'Täisarv '.$taisArv.'<br>';
+
+// kontrollime tüüpi
+echo gettype($sone);
