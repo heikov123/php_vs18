10d295301d509bc33dc599adc2cbb2b643f122a2
heiko.valli <heiko.valli@khk.ee>
2019-01-14 12:23:27 +0200

    Massiivid - 2d massiiv

--- arrays/massiivid.php
+++ arrays/massiivid.php
@@ -3,17 +3,36 @@ $kasutajad = array(
     array(
         'Alice',
         'Alice',
-        'Liddle'
+        'Liddle',
+        'female'
     ),
     array(
         'Bob',
         'Bob',
-        'Builder'
+        'Builder',
+        'male'
     )
 );
 
 
 echo '<hr>';
-echo '<pre>';
-print_r($kasutajad);
-echo '</pre>';
+echo '<table style="border: 1px solid">';
+echo '<tr>
+        <th>Kasutaja</th>
+        <th>Eesnimi</th>
+        <th>Perenimi</th>
+        <th>Sugu</th>
+      </tr>';
+foreach ($kasutajad as $kasutaja){
+    foreach ($kasutaja as $element){
+        for($i = 0; $i < count($kasutaja); $i++){
+            if($kasutaja[3] == 'female'){
+                echo '<td style="color: red">';
+            } else {
+                echo '<td style="color: blue; font-size: 40px">';
+            }
+        }
+        echo $kasutaja.'</td>';
+    }
+    echo '<hr>';
+}
