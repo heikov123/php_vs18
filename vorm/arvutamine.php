e4f065f78ff515f314e3753c4094f770982c54cf
heikov123 <heiko.valli@khk.ee>
2019-01-24 10:28:08 +0200

    Kujude ruumala arvutusued

--- vorm/arvutamine.php
+++ vorm/arvutamine.php
@@ -12,3 +12,13 @@ function andmeTootlus(&$vormiAndmed, $jaotus){
     }
     $vormiAndmed = array_slice($jaotatudAndmed,0,count($jaotatudAndmed));
 }
+// ruumala arvutused
+function keraRuumala($keraRaadius){
+    return 4/3 * pi() * pow($keraRaadius, 3);
+}
+function koonuseRuumala($koonuseRaadius, $koonuseKorgus){
+    return 1/3 * pi() * pow($koonuseRaadius, 2) * $koonuseKorgus;
+}
+function silindriRuumala($silindriRaadius, $silindriKorgus){
+    return pi() * pow($silindriRaadius, 2) * $silindriKorgus;
+}
