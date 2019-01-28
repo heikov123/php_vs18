8c5c19689cd5ac14f0b75fc33f334fccd25c7f3a
heiko.valli <heiko.valli@khk.ee>
2019-01-28 14:57:26 +0200

    Ajavööndi lisamine

--- date/index.php
+++ date/index.php
@@ -1,8 +1,9 @@
 <?php
 
+date_default_timezone_set('Europe/Tallinn');
 echo time();
 echo '<br>';
-echo date('d.m.Y G:i' , time());'<br>';
+echo date('d.m.Y G:i' , time());
 echo '<br>';
 echo date('d.m.Y G:i');
 echo '<br>';
