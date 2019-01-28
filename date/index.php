ec50c8df623603ee0648e347237d8cf6f93a85b5
heiko.valli <heiko.valli@khk.ee>
2019-01-28 15:00:03 +0200

    Pika kuupäeva eelistamine

--- date/index.php
+++ date/index.php
@@ -7,3 +7,15 @@ echo date('d.m.Y G:i' , time());
 echo '<br>';
 echo date('d.m.Y G:i');
 echo '<br>';
+echo  date('d.F.Y');
+echo '<br>';
+echo '<br>';
+// kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+// kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+// kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta
+?>
