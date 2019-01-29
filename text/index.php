2bd0ab229bb3e7be4e83f07846cee371d150526e
heiko.valli <heiko.valli@khk.ee>
2019-01-29 10:49:38 +0200

    Teksti pikkus

--- text/index.php
+++ text/index.php
@@ -9,3 +9,13 @@ echo strtoupper($tekst);
 $tekst = 'Life Is About Ignoring The Drama.';
 
 echo ucfirst(strtolower($tekst));
+
+// teksti pikkus
+echo '<br>';
+$tekst = 'Experience is the teacher of fools';
+echo strlen($tekst);			//34
+echo '<br>';
+echo str_word_count($tekst);	//6
+echo '<br>';
+
+// teksti kärpimine
