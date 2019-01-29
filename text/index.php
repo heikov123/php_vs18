944bb93e23da40e6a4ea25ff43f7f4ecb33927e8
heiko.valli <heiko.valli@khk.ee>
2019-01-29 10:44:33 +0200

    Teksti vormindamine

---
+++ text/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$tekst = 'Life Is About Ignoring The Drama.';
+
+echo strtolower($tekst);
+echo '<br>';
+echo strtoupper($tekst);
+
+$tekst = 'Life Is About Ignoring The Drama.';
+
+echo ucfirst(strtolower($tekst));
