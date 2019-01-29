99f40260ad98d2adcc136957f0fb489465aa5814
heiko.valli <heiko.valli@khk.ee>
2019-01-29 12:17:37 +0200

    Require

--- recode/sisu.php
+++ recode/sisu.php
@@ -1,7 +1,4 @@
-<?php include('13_includerequire_pais.php'); ?>
-    <div id="sisu">
-        <h2>Lorem ipsum</h2>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
-    </div>
-
-<?php include('13_includerequire_jalus.php'); ?>
+<?php
+include('13_includerequire_pais.php');
+include_once('13_includerequire_pais.php'); 	//ei käivitu, kuna on juba olemas
+?>
