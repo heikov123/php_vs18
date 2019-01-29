81d3724c0610d7fd7d5706c1748d0a6a85b15e68
heiko.valli <heiko.valli@khk.ee>
2019-01-29 10:50:48 +0200

    Teksti kärpimine

--- text/index.php
+++ text/index.php
@@ -19,3 +19,21 @@ echo str_word_count($tekst);	//6
 echo '<br>';
 
 // teksti kärpimine
+
+$tekst = ' 	A woman should soften but not weaken a man   ';
+echo "<pre>$tekst</pre>";
+echo "<pre>".trim($tekst)."</pre>";
+echo "<pre>".ltrim($tekst)."</pre>";
+echo "<pre>".rtrim($tekst)."</pre>";
+
+$tekst = 'A woman should soften but not weaken a man';
+echo trim($tekst, "A, a, k..n, w");	//oman should soften but not weake
+
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst); 	//Experience is the teacher of fools
+
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst, '<b>, <br>'); 	//<b>Experience</b> is the teacher <br>of fools
+
+echo '<br>';
+
