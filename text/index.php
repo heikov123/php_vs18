2fad4ecc5056d1a996632707f6495d22d2f04312
heiko.valli <heiko.valli@khk.ee>
2019-01-29 11:11:27 +0200

    Ülesanne 9

--- text/index.php
+++ text/index.php
@@ -1,112 +1,13 @@
 <?php
 
-$tekst = 'Life Is About Ignoring The Drama.';
+// ülesanne 9
 
-echo strtolower($tekst);
-echo '<br>';
-echo strtoupper($tekst);
-
-$tekst = 'Life Is About Ignoring The Drama.';
-
+$tekst = $_GET['tekst1'];
 echo ucfirst(strtolower($tekst));
-
-// teksti pikkus
 echo '<br>';
-$tekst = 'Experience is the teacher of fools';
-echo strlen($tekst);			//34
-echo '<br>';
-echo str_word_count($tekst);	//6
-echo '<br>';
+$tekst2 = $_GET['tekst2'];
+$punkt = strtoupper(implode('.',str_split($tekst2)));
+echo $punkt.'.';
 
-// teksti kärpimine
-
-$tekst = ' 	A woman should soften but not weaken a man   ';
-echo "<pre>$tekst</pre>";
-echo "<pre>".trim($tekst)."</pre>";
-echo "<pre>".ltrim($tekst)."</pre>";
-echo "<pre>".rtrim($tekst)."</pre>";
-
-$tekst = 'A woman should soften but not weaken a man';
-echo trim($tekst, "A, a, k..n, w");	//oman should soften but not weake
-
-$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
-echo strip_tags($tekst); 	//Experience is the teacher of fools
-
-$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
-echo strip_tags($tekst, '<b>, <br>'); 	//<b>Experience</b> is the teacher <br>of fools
-echo '<br>';
-
-// tekst kui massiiv
-
-$tekst = 'All thinking men are atheists';
-echo $tekst[0]; 				//A
-echo '<br>';
-echo $tekst[4]; 				//t
-
-$tekst = 'All thinking men are atheists';
-echo substr($tekst, 3, 5);		//thin
-echo '<br>';
-echo substr($tekst, 4, -13);	//thinking men
-echo '<br>';
-echo substr($tekst, -8, 7);		//atheist
-
-$tekst = 'All thinking men are atheists';
-print_r(str_word_count($tekst, 1));		//Array ( [0] => All [1] => thinking [2] => men [3] => are [4] => atheists )
-
-$tekst = 'All thinking men are atheists';
-$sona = str_word_count($tekst, 1);
-echo $sona[2];
-
-$tekst = 'All thinking men are atheists';
-print_r(str_word_count($tekst, 2));
-//Array ( [0] => All [4] => thinking [13] => men [17] => are [21] => atheists )
-echo '<br>';
-
-// tekstist otsimine
-
-$tekst = 'Happiness in intelligent people is the rarest thing I know.';
-$otsitav = 'in';
-$leia_tekstist = strpos($tekst, $otsitav, 0);	//4
-echo $leia_tekstist;
-
-$leia_tekstist = strpos($tekst, $otsitav, 6);	//10
-
-$tekst = 'Happiness in intelligent people is the rarest thing I know.';
-$otsitav = 'in';
-$nihe = 0;
-while($leia_tekstist = strpos($tekst, $otsitav, $nihe)){	//4 10 13 48
-    echo $leia_tekstist.'<br>';
-    $nihe = $leia_tekstist+strlen($otsitav);
-}
-echo '<br>';
-
-// teksti asendamine
-
-$tekst = 'Pai papa, pane paadile punased purjed peale';
-$asendus = 'emme';
-$otsitav_algus = 4;
-$otsitav_pikkus = 4;
-echo substr_replace($tekst, $asendus, $otsitav_algus, $otsitav_pikkus);
-
-$tekst = 'Pai papa, pane paadile punased purjed peale';
-$asendus = 'emme';
-$otsitav = 'papa';
-$nihe = 0;
-$asenduse_algus = strpos($tekst, $otsitav, $nihe);
-$asenduse_markide_arv = strlen($otsitav);
-echo substr_replace($tekst, $asendus, $asenduse_algus, $asenduse_markide_arv);
-
-$tekst = 'Musta lehma saba musta lehma taga, valge lehma saba valge lehma taga';
-$otsi = 'lehm';
-$asenda = 'koer';
-echo str_replace($otsi, $asenda, $tekst);
-
-$tekst = 'Musta lehma saba musta lehma taga, valge lehma saba valge lehma taga';
-$otsi = array('lehm', 'saba', 'taga');
-$asenda = array('koer', 'sarv', 'ees');
-echo str_replace($otsi, $asenda, $tekst);
-echo '<br><br>';
-
-
-
+?>
 
