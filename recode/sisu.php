c9d62c54ed4f06f1c96cabe5eeb3af9b11f608e2
heiko.valli <heiko.valli@khk.ee>
2019-01-29 12:13:05 +0200

    include

---
+++ recode/sisu.php
@@ -0,0 +1,7 @@
+<?php include('13_includerequire_pais.php'); ?>
+    <div id="sisu">
+        <h2>Lorem ipsum</h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
+    </div>
+
+<?php include('13_includerequire_jalus.php'); ?>
